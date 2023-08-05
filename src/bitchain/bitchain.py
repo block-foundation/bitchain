760e5e20e6028f15fe5432655c737f8d0bd6b048
Lars van Vianen <lars@vianen.com>
2023-08-05 09:47:40 +0200

    Update bitchain.py

--- src/bitchain/bitchain.py
+++ src/bitchain/bitchain.py
@@ -1,27 +1,35 @@
 import hashlib
 import json
-#import datetime
-
 from time import time
 
 
-class Bitchain(object):
-
+class Bitchain:
+    """
+    A simple Python blockchain implementation for educational purposes.
+    """
     def __init__(self):
+        """
+        Constructor for the Bitchain class. Initializes an empty chain and a
+        list of pending transactions.
+        Also adds the genesis block to the chain.
+        """
         self.chain = []
         self.pending_transactions = []
 
+        # Add the genesis block
         self.new_block(
-            previous_hash="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.", 
+            previous_hash="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks."  # noqa E501
             proof=100
-            )
-
-        #self.create_block(nonce = 1, previous_hash = '0')
-
-
-# Create a new block listing key/value pairs of block information in a JSON object. Reset the list of pending transactions & append the newest block to the chain.
+        )
 
     def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the blockchain.
+
+        :param proof: The proof returned by the Proof of Work algorithm
+        :param previous_hash: Hash of the previous Block in the chain
+        :return: New Block
+        """
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
@@ -29,32 +37,47 @@ class Bitchain(object):
             'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
+
         self.pending_transactions = []
         self.chain.append(block)
 
         return block
 
-#Search the blockchain for the most recent block.
-
     @property
     def last_block(self):
- 
+        """
+        Returns the last block in the blockchain.
+
+        :return: Block
+        """
         return self.chain[-1]
 
-# Add a transaction with relevant info to the 'blockpool' - list of pending tx's. 
-
     def new_transaction(self, sender, recipient, amount):
+        """
+        Adds a new transaction to the list of transactions.
+
+        :param sender: Address of the Sender
+        :param recipient: Address of the Recipient
+        :param amount: The amount
+        :return: The index of the Block that will hold this transaction
+        """
         transaction = {
             'sender': sender,
             'recipient': recipient,
             'amount': amount
         }
+
         self.pending_transactions.append(transaction)
+
         return self.last_block['index'] + 1
 
-# receive one block. Turn it into a string, turn that into Unicode (for hashing). Hash with SHA256 encryption, then translate the Unicode into a hexidecimal string.
-
     def hash(self, block):
+        """
+        Creates a SHA-256 hash of a Block.
+
+        :param block: Block
+        :return: str
+        """
         string_object = json.dumps(block, sort_keys=True)
         block_string = string_object.encode()
 
@@ -64,16 +87,26 @@ class Bitchain(object):
         return hex_hash
 
 
+def main():
+    """
+    Main function to interact with the blockchain.
+    """
+    blockchain = Bitchain()
 
-blockchain = Blockchain()
-t1 = blockchain.new_transaction("Satoshi", "Mike", '5 BTC')
-t2 = blockchain.new_transaction("Mike", "Satoshi", '1 BTC')
-t3 = blockchain.new_transaction("Satoshi", "Hal Finney", '5 BTC')
-blockchain.new_block(12345)
+    # Add transactions and blocks
+    blockchain.new_transaction("Satoshi", "Mike", '5 BTC')
+    blockchain.new_transaction("Mike", "Satoshi", '1 BTC')
+    blockchain.new_transaction("Satoshi", "Hal Finney", '5 BTC')
+    blockchain.new_block(12345)
 
-t4 = blockchain.new_transaction("Mike", "Alice", '1 BTC')
-t5 = blockchain.new_transaction("Alice", "Bob", '0.5 BTC')
-t6 = blockchain.new_transaction("Bob", "Mike", '0.5 BTC')
-blockchain.new_block(6789)
+    blockchain.new_transaction("Mike", "Alice", '1 BTC')
+    blockchain.new_transaction("Alice", "Bob", '0.5 BTC')
+    blockchain.new_transaction("Bob", "Mike", '0.5 BTC')
+    blockchain.new_block(6789)
 
-print("Genesis block: ", blockchain.chain)
+    # Print the chain
+    print("Genesis block: ", blockchain.chain)
+
+
+if __name__ == '__main__':
+    main()
